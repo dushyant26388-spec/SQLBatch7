107eaeac320c103bb825341abd201e3d04860bcb
Dushyant Sharma <dushyant26388@gmail.com>
2026-07-09 23:40:44 +0530

    SQL_8

---
+++ Sql_8.sql
@@ -0,0 +1,12 @@
+show databases;
+
+use world;
+
+describe country;
+
+select * from city;
+
+select * from city where population and population > 2000000;
+
+select * from city where name like '%a%';
+
